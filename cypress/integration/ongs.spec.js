bd0d8705bbf6411c7652edd93e6f822a55f520b5
leticiancalves <leticiancalves1@gmail.com>
2022-10-19 15:47:57 -0300

    first commit

---
+++ cypress/integration/ongs.spec.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" /> 
+
+
+
+
+describe('Ongs', () => {
+    it('deve poder realizar um cadastro', () => {
+        cy.visit('http://localhost:3000/register');
+        //cy.get busca um elemento
+        //type insere um texto
+        //it.skip ao invés do it => faz com que seja pausado o teste
+
+        cy.get('[data-cy=name]').type('Teste');
+        cy.get('[data-cy=email]').type('teste@gmail.com');
+        cy.get('[data-cy=whatsapp]').type('34988100000');
+        cy.get('[data-cy=city]').type('Araguari');
+        cy.get('[data-cy=uf]').type('MG');
+        //routing - escutar onde a app esta se conectando com as conexoes http e serve pra criaçao de mocks
+        //etapas: start server com o cy.server
+        //criar rota com cy.route()
+        //atribuir a um alias
+        //esperar com cy.wait e fazer uma validação
+
+        //cy.server() //inicia um servidor pra puxar respostas do cy.route e cy.request
+        cy.route('POST', '**/ongs').as('postOng') //cria rotas pra monitorar uma determinada requisiçao e criar mocks. como n sabemos a rota colocamos **/ongs. as salva as rotas pra uma variavel
+
+        cy.get('[data-cy=submit]').click();
+
+        cy.wait('@postOng').then((xhr) => { 
+            expect(xhr.status).be.eq(200);
+            expect(xhr.response.body).has.property('id');
+            expect(xhr.response.body.id).is.not.null;
+        })
+
+    });
+    it('deve poder realizar um login no sistema', () => {
+
+        cy.visit('http://localhost:3000/');
+        cy.get('input').type(Cypress.env('createdOngId'));
+        cy.get('.button').click();
+    });
+}); //coloca o que está testando, cada it vai ser um teste.
---
+++ cypress/support/commands.js
@@ -0,0 +1,47 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+
+//criar comandos customizados
+Cypress.Commands.add("createOng", () => {
+    cy.request({ 
+        method: 'POST',
+        url: 'http://localhost:3333/ongs',
+        body: {
+            name: 'teste',
+            email: 'teste@gmail.com',
+            whatsapp: '349855226525',
+            city: 'araguari',
+            uf: 'MG'
+
+        }
+    }).then(response => {
+        expect(response.body.id).is.not.null;
+        cy.log(response.body.id); 
+
+        Cypress.env('createdOngId', response.body.id) //cria variavel de ambiente temporaria pra ser usada durante o teste
+    }) 
+})
